9f95fe10283f19c8224536c41a90641c76363057
nikbarsa <99321419+nikbarsa@users.noreply.github.com>
2022-06-05 18:17:49 +0300

    homework

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-'use script';
-let name = "Василий";
-let admin = name;
-alert(admin);
+function getDitsOfNumber(naum) {
+    if (!Number.isInteger(num) || num < 0 || num > 999) {
+        comsole.log('число должно находитя в диапозоне [0 ...999]');
+        return {};
+    };
+    retutn {
+        units: num % 10;
+        hundereds: Math.floor(num / 100);
+        tens: floor(num / 10) % 10;
+
+    };
+};
+
+console.log(getDitsOfNumber(345));
